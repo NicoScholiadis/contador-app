012e47d488ebff2fad7ed103ec3ca405faefb24c
Nico Scholiadis <nicoscholiadis@gmail.com>
2023-06-14 19:36:18 -0300

    Primer commit

---
+++ app.js
@@ -0,0 +1,31 @@
+let count = 0;
+
+const value = document.querySelector('#value');
+const btns = document.querySelectorAll('.btn');
+
+console.log(btns);
+
+btns.forEach(function (btn){
+    btn.addEventListener("click", function(e){
+        const styles = e.currentTarget.classList;
+        if(styles.contains("decrease")){
+            count--;
+        }
+        else if(styles.contains("increment")){
+            count++;
+        }
+        else if(styles.contains("reset")){
+            count = 0;
+        }
+        if(count > 0){
+            value.style.color = "green";
+        }
+        if(count < 0){
+            value.style.color = "red";
+        }
+        if(count === 0){
+            value.style.color = "blue";
+        }
+        value.textContent = count;
+    })
+});
